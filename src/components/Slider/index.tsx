0f14898daca4a675ef1c0a0f4ae4faad10517dae
Cristofer Portela <cristoferportela@gmail.com>
2021-06-08 11:37:58 -0300

    refact: texts changed

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Header, Image } from './Slider.styled';
-import img1 from '../../assets/img/img-tpl-1.jpg';
+// import img1 from '../../assets/img/img-tpl-1.jpg';
+import img2 from '../../assets/img/pexels-vlada-karpovich-6114985.jpg';
 
 export const Slider = () => {
     return (
@@ -9,14 +10,15 @@ export const Slider = () => {
                 <div className="flex">
                     <Header className="flex-8">
                         <div>
-                            <h2>Titulo</h2>
-                            <p>Texto</p>
-                            <p>Teste</p>
+                            <h2>Lorem ipsum dolor sit amet.</h2>
+                            <p>Consectetur adipiscing elit. Mauris varius leo fringilla turpis ultricies, et viverra erat luctus. Ut elementum molestie nisl, id varius ipsum posuere commodo.</p>
+                            <p>Donec tortor velit, laoreet vel vulputate ac, scelerisque et sem. Sed at maximus lectus. Mauris porttitor turpis in mauris tincidunt accumsan. </p>
                         </div>
                     </Header>
                     <figure className="flex-9">
-                        <Image src={img1} alt="" />
+                        <Image src={img2} alt="" />
                     </figure>
+                    <p>Foto de Vlada Karpovich no Pexels</p>
                 </div>
             </section>
         </Container>
