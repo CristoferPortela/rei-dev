87a7794527970f0d84c356fd73a09a4423332f77
Cristofer Portela <cristoferportela@gmail.com>
2021-06-08 11:37:26 -0300

    feat: default button created

---
+++ src/components/Styled/Button.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+interface iButton {
+    border: string;
+    bg?: string;
+    color?: string;
+}
+
+export const Button = styled.button<iButton>`
+    border: 2px solid ${(props) => props.border};
+    padding: .5rem 1.3rem;
+    background-color: ${(props) => props.bg || "transparent"};
+    color: ${(props) => props.color || "black"};
+    cursor: pointer;
+`;
