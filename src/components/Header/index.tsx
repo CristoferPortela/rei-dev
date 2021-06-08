f0b0a00ee202a9b501a3704a8732969440dd1e19
Cristofer Portela <cristoferportela@gmail.com>
2021-06-08 11:38:22 -0300

    refact: using Row and FlexItem components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { HeaderStyled, NavMenu, Brand } from './Header.styled';
+import { NavMenu, Brand } from './Header.styled';
 import Logo from '../../assets/img/logo.webp';
+import { FlexItem, Row } from '../Styled/Flex';
+import { Button } from '../Styled/Button';
+import { Container } from '../Styled/Container';
 
 export const Header = () => {
     const pages = [
@@ -14,26 +17,52 @@ export const Header = () => {
         "Contato",
     ];
     return (
-        <HeaderStyled>
-            <Brand>
-                <Link to="/">
-                    {/* Rei dev */}
-                    <img src={Logo} alt="Rei-dev logo" />
-                </Link>
-            </Brand>
-            <NavMenu>
-                <ul>
-                    {
-                        pages.map((page) => (
-                            <li key={page}>
-                                <Link to="/">
-                                    {page}
-                                </Link>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </NavMenu>
-        </HeaderStyled>
+        <Container as={"header"} bg={"#fafbfc"}>
+
+            <Row align={"Center"}>
+                <FlexItem auto={true}>
+                    <Brand>
+                        <Link to="/">
+                            {/* Rei dev */}
+                            <img src={Logo} alt="Rei-dev logo" />
+                        </Link>
+                    </Brand>
+                </FlexItem>
+                <FlexItem size={6}>
+                    <NavMenu>
+                        <ul>
+                            {
+                                pages.map((page) => (
+                                    <li key={page}>
+                                        <Link to="/">
+                                            {page}
+                                        </Link>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </NavMenu>
+                </FlexItem>
+                <FlexItem size={3}>
+                    <Row>
+                        <FlexItem size={5}>
+                            <Button border={"rgb(84, 65, 160)"}>
+                                Botão 1
+                            </Button>
+                        </FlexItem>
+                        <FlexItem size={5}>
+                            <Button border={"rgb(84, 65, 160)"} bg={"rgb(84, 65, 160)"} color={"white"}>
+                                Botão 2
+                            </Button>
+                        </FlexItem>
+                        <FlexItem size={2}>
+                            <span className="material-icons">
+                                search
+                            </span>
+                        </FlexItem>
+                    </Row>
+                </FlexItem>
+            </Row>
+        </Container>
     )
 }
