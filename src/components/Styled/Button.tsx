3052ef6d48b3a9d27b829aadd268441f9d42bb6e
Cristofer Portela <cristoferportela@gmail.com>
2021-06-08 12:36:37 -0300

    refact: button padding resized

--- src/components/Styled/Button.tsx
+++ src/components/Styled/Button.tsx
@@ -9,7 +9,7 @@ interface iButton {
 
 export const Button = styled.button<iButton>`
     border: 2px solid ${(props) => props.border};
-    padding: .5rem 1.3rem;
+    padding: .5rem 1.5rem;
     background-color: ${(props) => props.bg || "transparent"};
     color: ${(props) => props.color || "black"};
     cursor: pointer;
