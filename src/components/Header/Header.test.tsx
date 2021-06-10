b59ecabc41187634c3f8465558e7f7a986877b44
cristofer portela <cristoferportela@gmail.com>
2021-06-09 23:07:23 -0300

    test: testing if has menu

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,15 +2,28 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Header } from '.';
+import { ThemeProvider } from 'styled-components';
+import { StyledTheme } from '../../StyledGlobal';
 
 const H = () => (
-    <Router>
-        <Header />
-    </Router>
+    <ThemeProvider theme={StyledTheme}>
+        <Router>
+            <Header />
+        </Router>
+    </ThemeProvider>
 )
 
 test('renders rei dev text', () => {
     render(<H />);
     const reiDevText = screen.getByAltText(/Rei-dev logo/i);
+    const menuHamburguer = screen.getByText(/menu/i);
     expect(reiDevText).toBeInTheDocument();
+    expect(menuHamburguer).toBeInTheDocument();
 });
+
+test('renders menu', () => {
+    render(<H />);
+    const menuHamburguer = screen.getByText(/menu/i);
+    expect(menuHamburguer).toBeInTheDocument();
+});
+
