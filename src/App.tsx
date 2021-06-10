73cb713d60b5b540ed41bdbed0b0c4f952864479
cristofer portela <cristoferportela@gmail.com>
2021-06-09 23:33:35 -0300

    --test-- image attribtte to slider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { GlobalStyle, StyledTheme } from "./StyledGlobal";
 import { Header } from "./components/Header";
 import { Slider } from "./components/Slider";
 import { Pages } from './Pages';
+import img2 from './assets/img/pexels-vlada-karpovich-6114985.jpg';
 import 'material-icons';
 import './style.sass';
 
@@ -15,7 +16,13 @@ function App() {
         <GlobalStyle />
         <Router>
           <Header />
-          <Slider />
+          <Slider images={[{
+            title: "Lorem ipsum dolor sit amet.",
+            imageUrl: img2,
+            text: `<p>Consectetur adipiscing elit. Mauris varius leo fringilla turpis ultricies, et viverra erat luctus. Ut elementum molestie nisl, id varius ipsum posuere commodo.</p>
+            <p>Donec tortor velit, laoreet vel vulputate ac, scelerisque et sem. Sed at maximus lectus. Mauris porttitor turpis in mauris tincidunt accumsan. </p>`,
+            author: "Foto de Vlada Karpovich no Pexels",
+          }]} />
           <Pages />
         </Router>
       </ThemeProvider>
