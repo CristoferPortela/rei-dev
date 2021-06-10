20df28bdd528a9660eb1009425a5dd708b0a3e8d
cristofer portela <cristoferportela@gmail.com>
2021-06-09 23:33:53 -0300

    feat: slider with prop mapping the images

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,29 +1,49 @@
 import React from 'react'
 import { Container, Header, Image } from './Slider.styled';
-// import img1 from '../../assets/img/img-tpl-1.jpg';
-import img2 from '../../assets/img/pexels-vlada-karpovich-6114985.jpg';
 
-export const Slider = () => {
+interface iImage {
+    title: string;
+    text?: string;
+    imageUrl?: string;
+    imageDescription?: string;
+    author?: string;
+}
+interface iSlider {
+    images: Array<iImage>;
+}
+
+export const Slider: React.FunctionComponent<iSlider> = (props) => {
     return (
         <Container>
             <section>
-                <div className={"flex flex-wrap-md"}>
-                    <Header className={"flex-8-lg flex-12"}>
-                        <div>
-                            <h2>Lorem ipsum dolor sit amet.</h2>
-                            <p>Consectetur adipiscing elit. Mauris varius leo fringilla turpis ultricies, et viverra erat luctus. Ut elementum molestie nisl, id varius ipsum posuere commodo.</p>
-                            <p>Donec tortor velit, laoreet vel vulputate ac, scelerisque et sem. Sed at maximus lectus. Mauris porttitor turpis in mauris tincidunt accumsan. </p>
+                {
+                    props.images.map((image) => (
+                        <div className={"flex flex-wrap-md"} key={image.title}>
+                            <Header className={"flex-8-lg flex-12"}>
+                                <div>
+                                    <h2>{image.title}</h2>
+                                    <div dangerouslySetInnerHTML={{ __html: image?.text ?? "" }} />
+                                </div>
+                            </Header>
+                            {
+                                image.imageUrl ?
+                                    <figure className={"flex-9-lg flex-12 w-100-md"}>
+                                        <Image src={image.imageUrl} alt={image?.imageDescription ?? image.title} />
+                                    </figure>
+                                    : null
+                            }
+                            {
+                                image.author ?
+                                    <p className={"flex-12"} style={{ textAlign: 'right' }}>
+                                        <small style={{ fontSize: 12 }}>
+                                            {image.author}
+                                        </small>
+                                    </p>
+                                    : null
+                            }
                         </div>
-                    </Header>
-                    <figure className={"flex-9-lg flex-12 w-100-md"}>
-                        <Image src={img2} alt="" />
-                    </figure>
-                    <p className={"flex-12"} style={{ textAlign: 'right' }}>
-                        <small style={{ fontSize: 12 }}>
-                            Foto de Vlada Karpovich no Pexels
-                        </small>
-                    </p>
-                </div>
+                    ))
+                }
             </section>
         </Container>
     )
