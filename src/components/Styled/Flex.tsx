f9e6d2443948586c8592d0df2173dabf93127823
cristofer portela <cristoferportela@gmail.com>
2021-06-12 22:31:33 -0300

    feat: flexItem with media screen

--- src/components/Styled/Flex.tsx
+++ src/components/Styled/Flex.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 interface iRow {
     align?: string;
+    justify?: string
 }
 
 export const Row = styled.div<iRow>`
@@ -10,14 +11,32 @@ export const Row = styled.div<iRow>`
     display: flex;
     flex-wrap: wrap;
     align-items: ${(props) => props.align || "flex-start"};
+    ${(props) => props.align ? `justify-content: ${props.align}` : ''};
 `;
 
+interface iMediaQuery {
+    query: number;
+    value: number | string;
+}
 interface iFlexItem {
-    size?: number;
-    auto?: boolean;
+    size?: string | number | iMediaQuery[];
 }
 
 export const FlexItem = styled.div<iFlexItem>`
-    width: ${(props) => props.auto ? "auto" : ((props?.size ?? 1) * 8.333).toFixed(3) + "%"};
-    flex: 0 0 ${(props) => props.auto ? "auto" :((props?.size ?? 1) * 8.333).toFixed(3) + "%"};
+    ${(props) => {
+        if (typeof props.size === 'number' || props.size === 'string') {
+            const val = typeof props.size === 'string' ? props.size : ((props?.size ?? 1) * 8.333).toFixed(3) + "%";
+            return (`
+                width: ${val};
+                flex: 0 0 ${val};
+            `)
+        } else if (Array.isArray(props.size)) {
+            return props?.size?.map((med) => (`
+                @media screen and (min-width: ${med.query}px) {
+                    width: ${typeof med.value === 'string' ? med.value : (med.value * 8.333).toFixed(3) + "%"};
+                    flex: 0 0 ${typeof med.value === 'string' ? med.value : (med.value * 8.333).toFixed(3) + "%"};
+                }
+            `))
+        }
+    }}
 `;
